40a15a890613a95c206ff16fa774536e081d07f5
Ruben Olano <rubendavid2711@gmail.com>
2022-09-06 12:10:22 -0500

    Update game to be able to restart

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+
 #[derive(Debug)]
 pub enum GameErr {
     UnableToFetchWords(io::Error),
@@ -15,6 +17,7 @@ pub enum GameErr {
 }
 
 struct Game {
+    dictionary: Vec<String>,
     word: String,
     guesses: usize,
     won: bool,
@@ -28,11 +31,7 @@ impl Game {
         // use the read_file function to get a vector of words
         match Game::read_file() {
             Ok(words) => {
-                // pick a random word from the vector
-                let rand_index = rand::thread_rng().gen_range(0..words.len());
-                let word = &words[rand_index];
-                // create a vector of underscores the same length as the word
-                // create a new game with the word, guesses, and underscores
+                let word = Game::get_word(words.clone());
                 Game {
                     word: word.to_string(),
                     guesses: 0,
@@ -40,18 +39,18 @@ impl Game {
                     lose: false,
                     underscores: vec!['_'; word.len()],
                     prev_guesses: Vec::new(),
+                    dictionary: words,
                 }
             }
-            Err(_) => {
-                Game {
-                    word: "".to_string(),
-                    guesses: 0,
-                    won: false,
-                    lose: false,
-                    underscores: Vec::new(),
-                    prev_guesses: Vec::new(),
-                }
-            }
+            Err(_) => Game {
+                word: "".to_string(),
+                guesses: 0,
+                won: false,
+                lose: false,
+                underscores: Vec::new(),
+                prev_guesses: Vec::new(),
+                dictionary: Vec::new(),
+            },
         }
     }
     fn read_file() -> Result<Vec<String>, GameErr> {
@@ -67,6 +66,15 @@ impl Game {
             }
             Err(e) => Err(GameErr::UnableToFetchWords(e)),
         }
+    }
+
+    fn get_word(words: Vec<String>) -> String {
+        // pick a random word from the vector
+        let rand_index = rand::thread_rng().gen_range(0..words.len());
+        let word = &words[rand_index];
+        // create a vector of underscores the same length as the word
+        // create a new game with the word, guesses, and underscores
+        word.to_string()
     }
 
     fn read_guess(&mut self) -> Result<(), GameErr> {
@@ -105,20 +113,20 @@ impl Game {
                 //Make sure the guess is only one character long
                 if guess.len() == 1 {
                     // Return single character guess to the game
-                    let guess_chars: char = match guess.chars().next() {
-                        Some(c) => c,
-                        None => return Err(GameErr::UnableToFetchGuess),
-                    };
-                    // Check if duplicate guess
-                    if self.prev_guesses.contains(&guess_chars) {
-                        println!("You already guessed that!");
-                    } else {
-                        // Add the guess to the list of previous guesses
-                        *exit = true;
-                        let chars: Vec<char> = guess.chars().collect();
-                        println!("You guessed: {}", chars[0]);
+                    if let Some(guess_chars) = guess.chars().next() {
                         // Check if the guess is in the word
-                        self.letters_in_word(chars[0])?;
+                        if self.prev_guesses.contains(&guess_chars) {
+                            println!("You already guessed that!");
+                        } else {
+                            // Add the guess to the list of previous guesses
+                            *exit = true;
+                            let chars: Vec<char> = guess.chars().collect();
+                            println!("You guessed: {}", chars[0]);
+                            // Check if the guess is in the word
+                            self.letters_in_word(chars[0])?;
+                        }
+                    } else {
+                        return Err(GameErr::UnableToFetchGuess);
                     }
                 } else {
                     println!("Please enter a single character!");
@@ -143,7 +151,11 @@ impl Game {
         Ok(())
     }
 
-    fn check_letters(&mut self, underscores: &mut Vec<char>, guess_lower: Vec<char>) -> Result<bool, GameErr> {
+    fn check_letters(
+        &mut self,
+        underscores: &mut Vec<char>,
+        guess_lower: Vec<char>,
+    ) -> Result<bool, GameErr> {
         let mut correct: bool = false;
         for a in 0..self.word.len() {
             let letter_lower: Vec<char> =
@@ -175,7 +187,7 @@ impl Game {
         let underscores: String = self.underscores.iter().collect();
         if underscores == self.word {
             self.won = true;
-            println!("\x1B[H\x1B[2J");
+            println!("{}", CLEAR);
             println!("You win! The word was {}", self.word);
         }
         if self.guesses > 5 {
@@ -183,18 +195,60 @@ impl Game {
             println!("Too many incorrect guesses! The word was {}", self.word);
         }
     }
+
+    pub fn restart_game(&mut self) {
+        // Clear the screen
+        println!("{}", CLEAR);
+        // Get a new word
+        let new_word = Game::get_word(self.dictionary.clone());
+        // Set the new word
+        self.word = new_word;
+        // Reset the guesses
+        self.guesses = 0;
+        // Reset the underscores
+        self.underscores = vec!['_'; self.word.len()];
+        // Reset the previous guesses
+        self.prev_guesses = Vec::new();
+        // Reset the win and lose flags
+        self.won = false;
+        self.lose = false;
+    }
 }
 
 pub fn run() -> Result<(), GameErr> {
     let mut game = Game::init();
     println!("Game Start!");
-    while !game.won && !game.lose {
-        println!("\x1B[H\x1B[2J");
+    loop {
+        while !game.won && !game.lose {
+            println!("\x1B[H\x1B[2J");
+            board::get_state(game.guesses);
+            game.read_guess()?;
+        }
         board::get_state(game.guesses);
-        game.read_guess()?;
+        if game.won || game.lose {
+            prompt_restart(&mut game)?;
+        }
     }
-    board::get_state(game.guesses);
-    Ok(())
 }
 
-
+fn prompt_restart(game: &mut Game) -> Result<(), GameErr> {
+    let mut restart: String = String::new();
+    print!("Play again? (y/n): ");
+    match io::stdout().flush() {
+        Ok(_) => {}
+        Err(e) => println!("Error flushing output: {}", e),
+    };
+    match io::stdin().read_line(&mut restart) {
+        Ok(_) => {
+            restart = restart.trim().to_string().to_lowercase();
+            if restart == "y" {
+                game.restart_game();
+            } else {
+                println!("Thanks for playing!");
+                std::process::exit(0);
+            }
+        }
+        Err(e) => println!("Error reading input: {}", e),
+    }
+    Ok(())
+}
